5d15c6f076bbb30fb9b1547460dae8aa647f8084
Jeff Xiong <gigix1980@gmail.com>
2022-10-10 10:50:42 +0800

    minor adjustment to UI

--- lib/design.dart
+++ lib/design.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-var standardPadding = const EdgeInsets.symmetric(vertical: 16, horizontal: 8);
+var standardPadding = const EdgeInsets.symmetric(vertical: 16, horizontal: 20);
 
 standardTextStyle(double? fontSize) {
   return TextStyle(fontFamily: 'DejaVuSans', fontSize: fontSize);
